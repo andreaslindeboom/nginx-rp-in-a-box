39a7310f5b2b7baaabfe609c46f366470c2c40cb
Andreas Lindeboom <andreaslindeboom@mdmail.nl>
2015-10-10 22:44:29 +0200

    Now adding SSH key to DigitalOcean droplet

--- terraform/nginx.tf
+++ terraform/nginx.tf
@@ -2,9 +2,15 @@ provider "digitalocean" {
     token = "${var.digitalocean.access_token}"
 }
 
+resource "digitalocean_ssh_key" "nginx" {
+    name = "${var.ssh_key.name}"
+    public_key = "${file(var.ssh_key.path)}"
+}
+
 resource "digitalocean_droplet" "nginx-rp" {
     image = "13820512"
     name = "nginx-rp"
     region = "ams3"
     size = "512mb"
+    ssh_keys = ["${digitalocean_ssh_key.nginx.id}"]
 }
